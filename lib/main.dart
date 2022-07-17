b2811032eea9d0db4d2d5664f4b26a025d1e0602
Gabriel Silva <tais_santana002@hotmail.com>
2022-07-17 18:49:12 -0300

    Botão de favoritos, função de adicionar e remover favoritos e Nova rota para ver favoritados

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Gerador de nomes Taís',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Gerador de nomes Taís'),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -33,27 +26,89 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
+  final favoritos = <WordPair>{};
+  final fonte = const TextStyle(fontSize: 18);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-    padding: const EdgeInsets.all(16.0),
-    itemBuilder: (context, i) {
-      if (i.isOdd) return const Divider(); /*2*/
-      
-      final index = i ~/ 2; 
-      //Se chegar no fim da lista, gerar mais 10 palavras e colocar na lista.
-      if (index >= _suggestions.length) {
-        _suggestions.addAll(generateWordPairs().take(10));
-      }
-      return ListTile(
-        title: Text(
-          _suggestions[index].asPascalCase,
-          style: _biggerFont,
-        ),
-      );
-    },
-  );
+    return Scaffold(
+      appBar: AppBar(  
+        title: const Text('Gerador de nomes Taís'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.favorite),
+            onPressed: _pushSaved,
+            tooltip: 'Mostrar Favoritos',
+          ),
+        ],
+      ),               
+      body: ListView.builder( 
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider(); /*2*/
+          
+          final index = i ~/ 2; 
+          //Se chegar no fim da lista, gerar mais 10 palavras e colocar na lista.
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          final jaFoiFavoritado = favoritos.contains(_suggestions[index]);
+
+          return ListTile(
+            title: Text(
+              _suggestions[index].asPascalCase,
+              style: fonte,
+            ),
+            trailing: Icon(   
+              jaFoiFavoritado ? Icons.favorite : Icons.favorite_border,
+              color: jaFoiFavoritado ? Colors.lightBlue : null,
+              semanticLabel: jaFoiFavoritado ? 'Remover favorito' : 'Favoritar',
+            ),
+            onTap: () {        
+              setState(() {
+                if (jaFoiFavoritado) {
+                  favoritos.remove(_suggestions[index]);
+                } else {
+                  favoritos.add(_suggestions[index]);
+                }
+              });
+            },         
+          );
+        },
+      ),
+    );
   }
+
+  void _pushSaved() {
+     Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = favoritos.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: fonte,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Lista de Favoritos'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
 }
